fb7cfa8ce3c99ea79c0958a9063ae793b5faf055
steve <stevedanko1@gmail.com>
2013-12-30 12:08:38 -0500

    set up differnt enviorments

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StaffContact::Application.config.secret_key_base = '8fd14b8c264689b54b9543a151110390d6ccc8cef4a202aa54c33efea6a0c5bcc603a916e1cb3623dbe06ad355a3bb859e6f7932a12dbb89cbbb24a418706d80'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StaffContact::Application.config.session_store :cookie_store, key: '_staff_contact_session'
